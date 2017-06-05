cc084f714cfd3a3c1e48bf7385aac9ce373bf1f7
aria-grande <christina.ohari@gmail.com>
2017-06-05 11:03:56 +0900

    [FIXED] popped view controller when clicking saving button adding new exercise.

--- PTYourself/Controller/AddNewExerciseViewController.swift
+++ PTYourself/Controller/AddNewExerciseViewController.swift
@@ -7,6 +7,12 @@ class AddNewExerciseViewController: UIViewController {
     @IBOutlet var newExercise: UITextField!
 
     @IBAction func saveNewExercise(_ sender: AnyObject) {
-        ModelManager.addExerciseToList(newExercise.text!)
+        if let exercise = newExercise.text, !exercise.isEmpty {
+            ModelManager.addExerciseToList(newExercise.text!)
+        }
+        else {
+            print("내용을 입력해 주세요")    // todo: make alert
+        }
+        self.navigationController?.popViewController(animated: true)
     }
 }
